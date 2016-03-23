91232a214cb8cb0f6e449dbbe89d185c3430cbb7
Michael J. Simons <michael@simons.ac>
2016-03-23 17:47:31 +0100

    Add a simple demo project
    The project is basically generated by start.spring.io and contains a simple Thymeleaf template, nothing else.

---
+++ demo/src/main/java/ac/simons/springio2016/demo/Application.java
@@ -0,0 +1,24 @@
+package ac.simons.springio2016.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@SpringBootApplication
+public class Application {
+
+    @Controller
+    static class DemoController {
+
+	@RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+	public String index() {
+	    return "index";
+	}
+    }
+
+    public static void main(String... args) {
+	SpringApplication.run(Application.class, args);
+    }
+}
