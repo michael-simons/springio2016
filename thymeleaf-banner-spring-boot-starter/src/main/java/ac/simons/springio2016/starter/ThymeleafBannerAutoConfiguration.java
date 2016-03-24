e459e36e23344bfd8e92cb06ce42e64f2d4b1d1f
Michael J. Simons <michael@simons.ac>
2016-03-24 13:43:53 +0100

    Make ThymeleafBannerAutoConfiguration conditional on SpringTemplateEngine
    We declared optional dependencies so it might be that there is no Thymeleaf on the class path and so we can not run the auto configuration.

--- thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/springio2016/starter/ThymeleafBannerAutoConfiguration.java
+++ thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/springio2016/starter/ThymeleafBannerAutoConfiguration.java
@@ -1,7 +1,10 @@
 package ac.simons.springio2016.starter;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring4.SpringTemplateEngine;
 
 @Configuration
+@ConditionalOnClass(SpringTemplateEngine.class)
 class ThymeleafBannerAutoConfiguration {
 }
