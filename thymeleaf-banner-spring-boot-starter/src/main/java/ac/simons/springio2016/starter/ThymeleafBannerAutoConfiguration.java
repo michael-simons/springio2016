00e081aad532fd6012f4425e9ad05e670649faa2
Michael J. Simons <michael@simons.ac>
2016-03-24 13:52:07 +0100

    Run auto configuration before ThymeleafAutoConfiguration
    The Spring Boot ThymeleafAutoConfiguration automatically adds custom dialects, so we make sure our dialect will be there when ThymeleafAutoConfiguration is applied.

--- thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/springio2016/starter/ThymeleafBannerAutoConfiguration.java
+++ thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/springio2016/starter/ThymeleafBannerAutoConfiguration.java
@@ -1,10 +1,13 @@
 package ac.simons.springio2016.starter;
 
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 
 @Configuration
 @ConditionalOnClass(SpringTemplateEngine.class)
+@AutoConfigureBefore(ThymeleafAutoConfiguration.class)
 class ThymeleafBannerAutoConfiguration {
 }
