0199d78ac35e8f5987026a1bea203f46b0c4cf75
Michael J. Simons <michael@simons.ac>
2016-03-24 14:56:23 +0100

    Deliver the ansi_up via our starter
    As it turns out, src/main/static/* from within starters is recognized as static resources as well, so we can deliver the nice little script with the starter. Being at it, we add an "enableAnsiBanners" element to our dialect that creates a script node containing the resource as well as the inline script.

--- thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/springio2016/starter/ThymeleafBannerDialect.java
+++ thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/springio2016/starter/ThymeleafBannerDialect.java
@@ -1,5 +1,6 @@
 package ac.simons.springio2016.starter;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -7,6 +8,7 @@ import org.thymeleaf.Arguments;
 import org.thymeleaf.dialect.AbstractDialect;
 import org.thymeleaf.dom.Element;
 import org.thymeleaf.dom.Node;
+import org.thymeleaf.dom.Text;
 import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.processor.element.AbstractMarkupSubstitutionElementProcessor;
 
@@ -27,6 +29,33 @@ class ThymeleafBannerDialect extends AbstractDialect {
     public Set<IProcessor> getProcessors() {
 	final Set<IProcessor> processors = new HashSet<>();
 	
+	processors.add(new AbstractMarkupSubstitutionElementProcessor("enableansibanners") {
+	    @Override
+	    protected List<Node> getMarkupSubstitutes(Arguments arguments, Element element) {
+		final Element script = new Element("script");
+		script.setAttribute("type", "text/javascript");
+		script.setAttribute("src", "/js/ansi_up.js");
+		
+		final Element ansi2html = new Element("script");
+		script.setAttribute("type", "text/javascript");				
+		ansi2html.addChild(new Text("\n"
+			+ "document.addEventListener('DOMContentLoaded', function(event) {\n"
+			+ "   var banner = document.getElementsByClassName('banner');\n"
+			+ "   for (var i = 0; i < banner.length; ++i) {\n"
+			+ "      banner[i].innerHTML = ansi_up.ansi_to_html(banner[i].innerHTML);\n"
+			+ "   }\n"
+			+ "});\n"
+		,null,null,true ));		
+		
+		return Arrays.asList(script, ansi2html);
+	    }
+
+	    @Override
+	    public int getPrecedence() {
+		return 1000;
+	    }	    
+	});
+		
 	processors.add(new AbstractMarkupSubstitutionElementProcessor("show") {
 	    @Override
 	    protected List<Node> getMarkupSubstitutes(Arguments arguments, Element element) {
