d10b11fb69f8f34d9403178a353173d6cce5b3b6
Michael J. Simons <michael@simons.ac>
2016-03-24 10:27:27 +0100

    Remove the test code
    We won't be doing any further testing in this project.

--- thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/springio2016/starter/SomeBean.java
+++
@@ -1,4 +0,0 @@
-package ac.simons.springio2016.starter;
-
-public class SomeBean {
-}
--- thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/springio2016/starter/ThymeleafBannerAutoConfiguration.java
+++ thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/springio2016/starter/ThymeleafBannerAutoConfiguration.java
@@ -1,12 +1,7 @@
 package ac.simons.springio2016.starter;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-class ThymeleafBannerAutoConfiguration {   
-    @Bean
-    public SomeBean someBean() {
-	return new SomeBean();		
-    }
+class ThymeleafBannerAutoConfiguration {
 }
