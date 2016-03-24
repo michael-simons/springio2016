ee990b21857c4a89d4f24e25ac3820a2edb09dbe
Michael J. Simons <michael@simons.ac>
2016-03-24 15:05:02 +0100

    Fix duplicate beans
    Problem here was: The name of the beans. The Bean instantiated by the old method is named "bannerSupplier", like the method and that name together with the type was a unique match. Not so if we change the name to "defaultBannerSupplier". Then we must make sure that "defaultBannerSupplier" is only called when there is no banner supplier already.
    
    I prefer explicit ordering in such cases, so i added @Order as well.

--- thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/springio2016/starter/ThymeleafBannerAutoConfiguration.java
+++ thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/springio2016/starter/ThymeleafBannerAutoConfiguration.java
@@ -3,10 +3,12 @@ package ac.simons.springio2016.starter;
 import java.util.ArrayList;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ResourceLoader;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -18,12 +20,15 @@ class ThymeleafBannerAutoConfiguration {
 
     @Bean
     @ConditionalOnProperty(name = "spring.main.banner-mode", havingValue = "off")
+    @Order(-20)
     public BannerSupplier emptyBannerSupplier() {
 	return args -> new ArrayList<>();
     }
 
     @Bean
-    public BannerSupplier bannerSupplier(
+    @ConditionalOnMissingBean(BannerSupplier.class)
+    @Order(-10)
+    public BannerSupplier defaultBannerSupplier (
 	    final ResourceLoader resourceLoader,
 	    final Environment environment
     ) throws ClassNotFoundException {
